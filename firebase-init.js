178a32585fabd4b8438aca63115421173aa99f3f
kumarvikash13652 <kumarvikash13652@gmail.com>
2026-06-23 11:22:23 +0530

    Add files via upload

--- firebase-init.js
+++ firebase-init.js
@@ -1,13 +1,14 @@
 // firebase-init.js
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyDVE5gYyGCgAlfHw82Apna6DsMY9zE2bGs",
+    authDomain: "lovelybites-e4d35.firebaseapp.com",
+    projectId: "lovelybites-e4d35",
+    storageBucket: "lovelybites-e4d35.firebasestorage.app",
+    messagingSenderId: "650974633550",
+    appId: "1:650974633550:web:14e277c8aef5db9ff4ed1f"
 };
 
+// Firebase initialize karna
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
